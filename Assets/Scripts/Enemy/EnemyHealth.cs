6af36a274da56c7269f246dd80de61088b37a169
LDV23 <LDV23@users.noreply.github.com>
2021-09-29 20:29:12 +0300

    Revert "Merge branch 'Albert' into main"
    This reverts commit 1bec75ce9621b5511e4c888236baa7742abab73a, reversing
    changes made to b97c268fc5d8efc72fb29020e5619cb905152560.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,7 +6,7 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int _health = 1;
 
-    private EnemySpawn _enemySpawn;
+    [SerializeField] private EnemySpawn _enemySpawn;
 
     void Awake()
     {
